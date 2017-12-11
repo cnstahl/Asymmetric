cad2dc7045de70a0dfde200f0b260b65764f517a
Charles Stahl <cnstahl@princeton.edu>
2017-12-11 13:47:44 -0500

    modularized site updating"

--- python/growthrates.py
+++ python/growthrates.py
@@ -3,6 +3,11 @@ import random
 
 #  Constraints
 #  
+#  S[-1] = S[0] + diff
+#  -> There are really only N-1 sites
+#
+#  Never  update S[-1] individually
+#  Always update S[-1] at the end
 
 
 
@@ -20,25 +25,30 @@ def reset_S(N, m):
         else: S[i] = S[i-1] + 1
     return S
 
-def brick_update_S(S):
+def site_update_S(S, site):
     N = len(S)
-    diff = S[-1] - S[0]
+    if site < 0 or site > N-2:  raise ValueError("Bad site")
+    if site == 0: 
+        diff = S[-1] - S[0]
+        return np.minimum(S[-2]+1-diff, S[1]+1)
+    return     np.minimum(S[site-1]+1,  S[site+1]+1) 
+
+def brick_update_S(S):
+    print("USES WRONG UPDATING SCHEME")
+    N = len(S)
     nextS = np.copy(S)
-    nextS[0] = np.minimum(S[-1]+1-diff, S[1]+1)
-    for idx, val in enumerate(S):
-        if (idx != 0) and (idx != N-1):
-            nextS[idx] = np.minimum(S[idx-1]+1, S[idx+1]+1)
-            if nextS[idx] < S[idx]: raise ValueError("Decreasing entropy")
-    nextS[-1] = nextS[0] + diff
+    for idx in range(N-1):
+        nextS[idx] = site_update_S(S, idx)
+        if nextS[idx] < S[idx]: raise ValueError("Decreasing entropy")
+    nextS[-1] = nextS[0] + S[-1] - S[0]
     return(nextS)
 
 def random_update_S(S):
     N = len(S)
     diff = S[-1] - S[0]
     i = random.randint(0,N-2)
-#     print(i)
-    if i == 0: S[0] = np.minimum(S[ -2]+1-diff, S[1]+1)
-    else:      S[i] = np.minimum(S[i-1]+1,      S[i+1]+1)
+#    print(i)
+    S[i] = site_update_S(S, i)
     S[-1] = S[0] + diff
     return(S)
 
