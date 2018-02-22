5d70e4d41ebe1cb731cc8c27c3bf9947012ab649
Charles Stahl <cnstahl@princeton.edu>
2018-02-22 11:37:02 -0500

    Working on Hamiltonian

---
+++ python/hamiltonian.py
@@ -0,0 +1,83 @@
+import numpy as np
+import scipy.linalg as  la
+
+I = np.array([[ 1,  0],[ 0,  1]])
+X = np.array([[ 0,  1],[ 1,  0]])
+Y = np.array([[ 0,-1j],[1j,  0]])
+Z = np.array([[ 1,  0],[ 0, -1]])
+H3mult = np.array([[0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 , 0],
+               [0 ,  0 ,  1 ,  0 , -1 ,  0 ,  0 , 0],
+               [0 , -1 ,  0 ,  0 ,  1 ,  0 ,  0 , 0],
+               [0 ,  0 ,  0 ,  0 ,  0 , -1 ,  1 , 0],
+               [0 ,  1 , -1 ,  0 ,  0 ,  0 ,  0 , 0],
+               [0 ,  0 ,  0 ,  1 ,  0 ,  0 , -1 , 0],
+               [0 ,  0 ,  0 , -1 ,  0 ,  1 ,  0 , 0],
+               [0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 , 0]])
+
+def sparse_Hmult(l):
+    if (l==3): return H3mult
+#     print(np.shape(np.kron(sparse_Hmult(l-2),I2)))
+#     print(np.shape(np.kron(np.eye(l-3), H3mult)))
+    return (np.kron(sparse_Hmult(l-2), np.eye(4)) + 
+            np.kron(np.eye(2**(l-3)), H3mult))
+
+def sparse_H(l):
+    if (l%2 != 1): assert False, "l must be odd"
+    H = sparse_Hmult(l)
+    return H * (2j * np.pi)/(3 * np.sqrt(3))
+
+def dense_Hmult(l):
+    if (l==3): return H3mult
+#     print(np.shape(np.kron(sparse_Hmult(l-2),I2)))
+#     print(np.shape(np.kron(np.eye(l-3), H3mult)))
+    return (np.kron(dense_Hmult(l-1), np.eye(2)) + 
+            np.kron(np.eye(2**(l-3)), H3mult))
+
+def dense_H(l):
+    if (l < 3): assert False, "l must be >=3"
+    H = dense_Hmult(l)
+    return H * (2j * np.pi)/(3 * np.sqrt(3))
+
+def chop(a):
+    if not np.all(np.isclose(np.imag(a),0)): 
+        print("\nchop() removed the imaginary part\n ")
+    A = np.round(np.real(a),3)
+    if np.all(np.isclose(A.astype(int), A)): return A.astype(int)
+    else: return A
+
+def norm(A):
+    norm = np.trace(np.matmul(A, A.T.conj()))/(len(A))
+    assert np.isclose(np.imag(norm),0)
+    return np.real(norm)
+
+# Trace out the last/first i spins
+def end_trace(x, i):
+    N = len(x)
+    untraced = (int) (N/2**i)
+    traced = 2**i
+    x = x.reshape((untraced, traced, untraced, traced))
+    return np.trace(x, axis1=1, axis2=3)/2**i
+
+def front_trace(x, i):
+    N = len(x)
+    untraced = (int) (N/2**i)
+    traced = 2**i
+    x = x.reshape((traced, untraced, traced, untraced))
+    return np.trace(x, axis1=0, axis2=2)/2**i
+
+def tensor(*args):
+    A = 1
+    for arg in args:
+        A = np.kron(A, arg)
+    return A
+
+def par_tr(x,i):
+    N = len(x)
+    assert 2**i < N
+    
+    indices = np.array(range(N))
+    bit = int((N/2)/2**i)
+    mask = N - (bit) - 1
+    indices = indices & mask
+    return (x[:,np.unique(indices)][np.unique(indices)] + 
+            x[:,np.unique(indices + bit)][np.unique(indices + bit)])/2
---
+++ python/timeHamiltonian.py
@@ -0,0 +1,47 @@
+import numpy as np
+import scipy.linalg as  la
+import hamiltonian as hm
+
+L = 9
+
+H = hm.sparse_H(L)
+vals, vecs = la.eigh(H)
+eners = np.diag(vals)
+vecsd = vecs.T.conj()
+
+# Total time elapsed
+end = 3
+# Time steps per second
+n = 20
+N = n*end
+A = hm.Z
+for i in range(L-1):
+    A = np.kron(A,hm.I)
+B = hm.Z
+for i in range(L-1):
+    B = np.kron(hm.I,B)
+
+weightfore9 = np.empty((L, N))
+weightback9 = np.empty((L, N))
+
+for i in np.arange(N):
+    t = i/n
+    unitt = np.matmul(np.matmul(vecs,  np.diag(np.exp(-1j*vals*t))), vecsd)
+#    unitt = la.expm(-1j*H*t)
+#    At    = np.dot(np.dot(np.linalg.inv(unitt), A),             unitt)
+#    Bt    = np.dot(np.dot(np.linalg.inv(unitt), B),             unitt)
+    At    = np.matmul(np.matmul(np.linalg.inv(unitt), A),             unitt)
+    Bt    = np.matmul(np.matmul(np.linalg.inv(unitt), B),             unitt)
+#     print(chop(unit3t),"\n")
+    front = hm.norm(At)
+    back  = hm.norm(Bt)
+    
+    for j in range(L):
+        At = hm.end_trace(At,1)
+        Bt = hm.front_trace(Bt,1)
+        fronthere = hm.norm(At)
+        backhere  = hm.norm(Bt)
+        weightfore9[L-1-j, i] = front - fronthere
+        weightback9[j, i]     = back  - backhere
+        front = fronthere
+        back  = backhere
