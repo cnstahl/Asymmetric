a97bcad1cf608a540d5240622ecd439ab9b09c79
Charles Stahl @perseus <cnstahl@princeton.edu>
2018-02-26 13:01:54 -0500

    fixed plotweights

--- python/Hamiltonian/plotweights.py
+++ python/Hamiltonian/plotweights.py
@@ -8,8 +8,11 @@ weights = np.load(name)
 
 for weightlist in weights:
     L = len(weightlist)
+    ax = plt.subplot(111)
     for i in range(L):
-        plt.plot(np.arange(len(weightlist[i])),weightlist[i], label = str(i))
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2)
+        ax.plot(np.arange(len(weightlist[i])),weightlist[i], label = str(i))
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.9, box.height])
+    ax.legend(bbox_to_anchor=(1.05, 1), loc=2)
     plt.ylim(0,1)
     plt.show()
