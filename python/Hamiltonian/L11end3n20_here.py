3ae7b0110581f5b6725835d9f82710ed68ffdae9
Charles Stahl @perseus <cnstahl@princeton.edu>
2018-02-28 14:07:21 -0500

    weights at sites, tracking npy files

---
+++ python/Hamiltonian/L11end3n20_here.py
@@ -0,0 +1,55 @@
+import numpy as np
+import scipy.linalg as  la
+import hamiltonian as hm
+from sys import argv
+
+L = 11
+
+H = hm.sparse_H(L)
+vals, vecs = la.eigh(H)
+vecsd = vecs.T.conj()
+
+# Total time elapsed
+end = 3
+# Time steps per second
+n = 20
+N = n*end
+A = np.array([hm.Z[0,0], hm.Z[1,1]])
+for i in range(L-1):
+    A = np.kron(A,np.array([1,1]))
+Alist = hm.arr2list(A)
+B = np.array([hm.Z[0,0], hm.Z[1,1]])
+for i in range(L-1):
+    B = np.kron(np.array([1,1]),B)
+Blist = hm.arr2list(B)
+
+
+weightfore = np.empty((L, N))
+weightback = np.empty((L, N))
+
+for i in np.arange(N):
+    t = i/n
+    unitt = np.matmul(vecs * np.exp(-1j*vals*t), vecsd)
+    uninv = np.matmul(vecs * np.exp( 1j*vals*t), vecsd)
+    
+    ulist = hm.mat2list(unitt)
+    uinvlist = hm.mat2list(uninv)
+    
+    Atlist = []
+    for idx, val in enumerate(Alist):
+        Atlist.append(np.matmul(uinvlist[idx] * val, ulist[idx]))
+    Btlist = []
+    for idx, val in enumerate(Blist):
+        Btlist.append(np.matmul(uinvlist[idx] * val, ulist[idx]))
+    At = hm.list2mat(Atlist)
+    Bt = hm.list2mat(Btlist)
+
+    for j in range(L):
+        Aj = hm.par_tr(At,j)
+        Bj = hm.par_tr(Bt,L-j-1)
+        fronthere = hm.norm(Aj)
+        backhere  = hm.norm(Bj)
+        weightfore[L-1-j, i] = fronthere
+        weightback[j, i]     = backhere
+
+np.save("data/" + argv[0].replace(".py", ""), [weightfore, weightback])
