2bd97a6eff4fc437d03c7dd1333e83756c66eeda
Charles Stahl <cnstahl@princeton.edu>
2018-03-15 16:05:17 -0400

    removed comments from hamiltonian.py, added sparsehamiltonian.py

--- python/Hamiltonian/sparsehamiltonian.py
+++ python/Hamiltonian/sparsehamiltonian.py
@@ -1,12 +1,14 @@
 import numpy as np
-import scipy.linalg as  la
+import scipy.sparse.linalg as  la
+import scipy.linalg as npla
 import scipy.special
+import scipy.sparse as sp
 
-I = np.array([[ 1,  0],[ 0,  1]])
-X = np.array([[ 0,  1],[ 1,  0]])
-Y = np.array([[ 0,-1j],[1j,  0]])
-Z = np.array([[ 1,  0],[ 0, -1]])
-H3mult = np.array([[0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 , 0],
+I = sp.csc_matrix([[ 1,  0],[ 0,  1]])
+X = sp.csc_matrix([[ 0,  1],[ 1,  0]])
+Y = sp.csc_matrix([[ 0,-1j],[1j,  0]])
+Z = sp.csc_matrix([[ 1,  0],[ 0, -1]])
+H3mult = sp.csc_matrix([[0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 , 0],
                [0 ,  0 ,  1 ,  0 , -1 ,  0 ,  0 , 0],
                [0 , -1 ,  0 ,  0 ,  1 ,  0 ,  0 , 0],
                [0 ,  0 ,  0 ,  0 ,  0 , -1 ,  1 , 0],
@@ -19,8 +21,8 @@ def sparse_Hmult(l):
     if (l==3): return H3mult
 #     print(np.shape(np.kron(sparse_Hmult(l-2),I2)))
 #     print(np.shape(np.kron(np.eye(l-3), H3mult)))
-    return (np.kron(sparse_Hmult(l-2), np.eye(4)) + 
-            np.kron(np.eye(2**(l-3)), H3mult))
+    return (sp.kron(sparse_Hmult(l-2), sp.csc_matrix(np.eye(4))) + 
+            sp.kron(np.eye(2**(l-3)), H3mult))
 
 def sparse_H(l):
     if (l%2 != 1): assert False, "l must be odd"
@@ -31,8 +33,8 @@ def dense_Hmult(l):
     if (l==3): return H3mult
 #     print(np.shape(np.kron(sparse_Hmult(l-2),I2)))
 #     print(np.shape(np.kron(np.eye(l-3), H3mult)))
-    return (np.kron(dense_Hmult(l-1), np.eye(2)) + 
-            np.kron(np.eye(2**(l-3)), H3mult))
+    return (sp.kron(dense_Hmult(l-1), sp.csc_matrix(np.eye(2))) + 
+            sp.kron(np.eye(2**(l-3)), H3mult))
 
 def dense_H(l):
     if (l < 3): assert False, "l must be >=3"
@@ -47,7 +49,8 @@ def chop(a):
     else: return A
 
 def norm(A):
-    norm = np.trace(np.matmul(A, A.T.conj()))/(len(A))
+    norm = np.trace(np.matmul(A, A.T.conj()))/(A.shape[0])
+#    norm = (A.dot(A.T.conj())).diagonal().sum()/(len(A))
 #    print("here")
     assert np.isclose(np.imag(norm),0)
     return np.real(norm)
@@ -95,7 +98,7 @@ def permutations(L):
 
 def mat2list(matrix):
     assert (len(np.shape(matrix)) == 2)
-    L = (int) (np.log2(len(matrix)))
+    L = (int) (np.log2(matrix.shape[0]))
     alph2Sz, Sz2alph = permutations(L)
     diag = matrix[alph2Sz]
     diag = diag[:,alph2Sz]
@@ -110,7 +113,8 @@ def mat2list(matrix):
 def list2mat(A):
     L = len(A) - 1
     alph2Sz, Sz2alph = permutations(L)
-    diag = la.block_diag(*A)
+#    diag = sp.block_diag((*A), format='csc')
+    diag = npla.block_diag(*A)
     mat = diag[Sz2alph]
     return mat[:,Sz2alph]
 
@@ -201,7 +205,12 @@ def get_all_weights(L, end, n, here=True, dense = False):
     vecs_list = []
     vecsd_list = []
     for idx, H in enumerate(Hlist):
-        vals, vecs = la.eigh(H)
+        if (H.shape[0] == 1):
+            vals = H.todense()
+            vecs = np.array([[1]])
+        else:
+            vals, vecs = la.eigsh(H, k=H.shape[0]-2)
+#        vals, vecs = npla.eigh(H.todense())
         vals_list.append(vals)
         vecs_list.append(vecs)
         vecsd_list.append(vecs.T.conj())
