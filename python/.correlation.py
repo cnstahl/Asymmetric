6911d84196c3aacc51118ffaeab7e5a68d822046
Charles Stahl @perseus <cnstahl@princeton.edu>
2018-04-20 03:15:54 -0400

    corr runs

--- python/.correlation.py
+++ python/.correlation.py
@@ -23,44 +23,50 @@ def get_correlator(S):
     return Cij/(N-1)
 
 N      = 101
-init   = 600
+init   = 400
 n      = 1
 steps  = 1
-trials = 1000
+trials = 10000
 Nms    = 21
 finalCors1   = np.zeros((trials, Nms))
 ms = np.linspace(-1, 1, Nms)
 for i in range(Nms):
-    for l in range(trials):
-        S = reset_S(N, ms[i])
-        for j in range(init):
+    S = reset_S(N, ms[i])
+    for j in range(init):
+        S = stair_update_S(S, n)
+    for j in range(trials):
+        for k in range(steps):
             S = stair_update_S(S, n)
-        finalCors1[l, i] = get_correlator(S) - ms[i]**2
+        finalCors1[j, i] = get_correlator(S) - ms[i]**2
 
 n = 2
 finalCors2   = np.zeros((trials, Nms))
 for i in range(Nms):
-    for l in range(trials):
-        S = reset_S(N, ms[i])
-        for j in range(init):
+    S = reset_S(N, ms[i])
+    for j in range(init):
+        S = stair_update_S(S, n)
+    for j in range(trials):
+        for k in range(steps):
             S = stair_update_S(S, n)
-        finalCors2[l, i] = get_correlator(S) - ms[i]**2
+        finalCors2[j, i] = get_correlator(S) - ms[i]**2
 
 n = 3
 finalCors3   = np.zeros((trials, Nms))
 for i in range(Nms):
-    for l in range(trials):
-        S = reset_S(N, ms[i])
-        for j in range(init):
+    S = reset_S(N, ms[i])
+    for j in range(init):
+        S = stair_update_S(S, n)
+    for j in range(trials):
+        for k in range(steps):
             S = stair_update_S(S, n)
-        finalCors3[l, i] = get_correlator(S) - ms[i]**2
+        finalCors3[j, i] = get_correlator(S) - ms[i]**2
 
-plt.errorbar(ms, np.mean(finalCors3, axis=0), yerr=np.std(finalCors3, axis=0)/np.sqrt(trials-1), label="3-Stairs", fmt='-o')
-plt.errorbar(ms, np.mean(finalCors2, axis=0), yerr=np.std(finalCors2, axis=0)/np.sqrt(trials-1), label="2-Stairs", fmt='-o')
-plt.errorbar(ms, np.mean(finalCors1, axis=0), yerr=np.std(finalCors1, axis=0)/np.sqrt(trials-1), label="1-Stairs", fmt='-o')
+plt.errorbar(ms, np.mean(finalCors3, axis=0), yerr=np.std(finalCors3, axis=0), label="3-Stairs", fmt='-o')
+plt.errorbar(ms, np.mean(finalCors2, axis=0), yerr=np.std(finalCors2, axis=0), label="2-Stairs", fmt='-o')
+plt.errorbar(ms, np.mean(finalCors1, axis=0), yerr=np.std(finalCors1, axis=0), label="1-Stairs", fmt='-o')
 plt.plot(ms, .15-.15*(ms)**2, label="Analytic")
 plt.legend(loc=2)
 plt.xlabel("Slope")
 plt.ylabel("Correlation")
 plt.title("Initial and Final Correlations in 3-stair Circuit")
-plt.savefig("../figures/correlations/beststairCorrel.pdf")
+plt.savefig("../figures/correlations/goodstairCorrel.pdf")
