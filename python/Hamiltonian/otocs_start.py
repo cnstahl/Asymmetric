d6b8e5bd69da2f36d28196f35c7a81ec125fb24f
Charles Stahl <cnstahl@princeton.edu>
2018-10-29 16:09:04 -0400

    lots of data

--- python/Hamiltonian/otocs_start.py
+++ python/Hamiltonian/otocs_start.py
@@ -12,7 +12,7 @@ dense = True
 vs    = np.asarray([1, 2, 3, 4, 6, 8, 12, 16, 18, 24])
 #vs    = np.asarray([1, 3, 5, 7, 9, 12, 15, 18, 20])
 sites = np.arange(L)
-pert_strength = 4
+pert_strength = 0
 h = 0
 _, _, _, sig_z_list = qm.get_sigma_lists(L)
 
@@ -25,6 +25,7 @@ else:
 if (not pert_strength == 0):
     prefix = prefix + "_pert_"
 prefix = prefix + "h" + str(h)
+prefix = prefix + "p" + str(pert_strength)
 H = H + asym.init_pert(L, pert_strength)
 H = H + asym.finl_pert(L, pert_strength)
 H = H + qm.get_local_field(sig_z_list, np.random.rand(L)*2*h - h)
