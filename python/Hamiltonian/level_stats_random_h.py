5080e8adae2f5a82e59d18440eb0241c38b7f265
Charles Stahl <cnstahl@princeton.edu>
2018-11-02 17:10:52 +0000

    figures, level stats

--- python/Hamiltonian/level_stats_random_h.py
+++ python/Hamiltonian/level_stats_random_h.py
@@ -1,8 +1,8 @@
-DEPRECATED!! See notebook
 import numpy as np
-import matplotlib.pyplot as plt
 import quantum as qm
 import asymmetric as asym
+import scipy.sparse as sparse
+import scipy.linalg as la
 
 L = 11
 trials = 20
@@ -10,26 +10,26 @@ levels = 10
 rs = np.zeros((trials, levels))
 hs = np.linspace(1,10, levels)
 
-# Only create these once
-H0 = asym.dense_H(L)
-_, _, _, sig_z_list = qm.get_sigma_lists(L)
+levels = 25
+Ls = [8, 10, 12, 14]
+data = np.zeros((len(Ls),2,levels))
+for i, L in enumerate(Ls):
 
-for idx, h in enumerate(hs):
-    for j in range(trials):
-        H_pert = qm.get_local_field(sig_z_list, np.random.rand(L)*2*h - h)
-        H = H0 + H_pert
-        rs[j, idx] = qm.get_r(asym.mat2list(H)[L//2], avg=True)
+    trials = 260 - 20*L
+    rs = np.zeros((trials, levels))
+    hs = np.logspace(-1,2, levels)
 
-mean, std = qm.mean_and_std(rs)
-plt.errorbar(
-    hs,
-    mean,
-    yerr = std,
-    marker = '.',
-    drawstyle = 'steps-mid-'
-)
-plt.xlabel('$h$', fontsize=15)
-plt.ylabel('$r$', fontsize=15)
-plt.ylim(.4,.6)
-plt.title("Level repulsion" + ", L=" + str(L), fontsize=15)
-plt.savefig("figures/levelrepul" + "L" + str(L) + "small_.pdf")
+    # Only create these once per L
+    H0 = asym.dense_H(L)
+    _, _, _, sig_z_list = ([sig/2 for sig in sigs] for sigs in qm.get_sigma_lists(L))
+
+    for idx, h in enumerate(hs):
+        for j in range(trials):
+            H_pert = qm.get_local_field(sig_z_list, np.random.rand(L)*2*h - h)/2
+            H = H0 + H_pert
+            choose = L//2
+            rs[j, idx] = qm.get_r(asym.mat2list(H)[choose].A, nonz=True)
+
+    data[i] = qm.mean_and_std(rs)
+
+np.save("data/phasetrans", data)
