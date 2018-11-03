7d9326c685b85f4daf91b45cc639f28349fd4010
Charles Stahl <cnstahl@princeton.edu>
2018-11-03 14:39:46 -0400

    data

--- python/Hamiltonian/find_vec_size.py
+++ python/Hamiltonian/find_vec_size.py
@@ -18,7 +18,7 @@ def zotoc_vec_std(Hlist, vecs, Zlists, sites, t):
         OTOCs[idx] = [(v2.conj().T@v1).real for (v1, v2) in zip(v1s, v2s)]
     return OTOCs
 
-L = 10
+L = 12
 _,_,_, z_list = qm.get_sigma_lists(L, half=False)
 H = asym.dense_H(L)
 h = .1
@@ -27,7 +27,7 @@ H = H + qm.get_local_field(z_list, np.random.rand(L)*2*h - h)
 Hlist  = asym.mat2list(H)
 Zlists = [asym.mat2list(Z) for Z in z_list]
 
-nvecs = 5
+nvecs = 15
 sites = np.arange(L)
 t = 20
 OTOCss = np.zeros((nvecs,L,L+1))
